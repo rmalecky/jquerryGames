6b5ffa63b26d84b9a916c690ca7f1130baea8724
Sterling Forest / Ryan Malecky <rmalecky@gmail.com>
2013-09-25 11:13:28 -0400

    first commit

---
+++ TTT_UI.js
@@ -0,0 +1,16 @@
+(function (root) {
+  $(document).ready(function() {
+    var i = 0;
+    var j = 0;
+    // $("div").forEach(function(div){
+    //   dvi.attr("data-id", i + "," + j)
+    //   i++;
+    //   if (i > 2 ) {
+    //     j++;
+    //     i = 0;
+    //   }
+    // });
+    alert("working");
+  });
+
+})(this);
